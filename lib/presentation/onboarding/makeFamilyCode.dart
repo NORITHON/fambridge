98843feda197e8c47d17845b2ea0763bb0163fcc
zho <71474242+zhoho@users.noreply.github.com>
2023-01-28 06:07:34 +0900

    Update makeFamilyCode.dart

--- lib/presentation/onboarding/makeFamilyCode.dart
+++ lib/presentation/onboarding/makeFamilyCode.dart
@@ -3,5 +3,5 @@ import 'package:uuid/uuid.dart';
 void main() {
   var uuid = const Uuid();
   var v4 = uuid.v4();
-  var Familycode = v4.substring(31); // -> '110ec58a-a0f2-4ac4-8393-c866d813b8d1'
+  var Familycode = v4.substring(31); // -> 5자리 코드 자도 생성
 }
