467da5984b6fb33eda50b278ddf42c2fe64844cd
dmsvk0217 <dmsvk0217@gmail.com>
2023-01-27 13:10:26 +0900

    merge from head

--- lib/model/question.dart
+++ lib/model/question.dart
@@ -1,13 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fambridge/constants/database_fieldname/firebase_fieldname.dart';
 
-<<<<<<< HEAD
 class Question {
-=======
-import '../constants/database_fieldname/firebase_fieldname.dart';
-
-class Question{
->>>>>>> main
   final String questionId;
   final String questionScript;
   final Timestamp questionCreatedTimeStamp;
