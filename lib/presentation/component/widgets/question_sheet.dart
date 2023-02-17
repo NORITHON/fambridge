5d8d22e4a58b004c0b95d43cf2f7dd997ceeef1d
Zho <chlwlgh1011@naver.com>
2023-02-17 15:50:40 +0900

    edit_answer_list need to edit

--- lib/presentation/component/widgets/question_sheet.dart
+++ lib/presentation/component/widgets/question_sheet.dart
@@ -4,7 +4,6 @@ import 'package:wrapped_korean_text/wrapped_korean_text.dart';
 import '../../../model/group.dart';
 import '../../resources/color_manager.dart';
 import '../../resources/styles_manager.dart';
-import 'answer_progress_indicator_with_title.dart';
 
 class QuestionSheet extends StatefulWidget {
   const QuestionSheet({
@@ -46,8 +45,8 @@ class _QuestionSheetState extends State<QuestionSheet> {
             style: getMediumStyle(color: ColorManager.black, fontSize: 20),
           ),
         ),
-        const SizedBox(height: 35),
-        AnswerProgressIndicatorWithTitle(group: widget.group),
+        const SizedBox(height: 20),
+        //AnswerProgressIndicatorWithTitle(group: widget.group),
       ],
     );
   }
