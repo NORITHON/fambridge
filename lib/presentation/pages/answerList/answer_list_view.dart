bf47df167a4ad86d5e41c03c1cb785d19df3dd63
dmsvk0217 <dmsvk0217@gmail.com>
2023-02-18 22:07:04 +0900

    answerlistTabbar 모듈화

--- lib/presentation/pages/answerList/answer_list_view.dart
+++ lib/presentation/pages/answerList/answer_list_view.dart
@@ -1,3 +1,4 @@
+import 'package:fambridge/presentation/component/bar/category_list.dart';
 import 'package:fambridge/presentation/resources/assets_manager.dart';
 import 'package:fambridge/presentation/resources/color_manager.dart';
 import 'package:fambridge/presentation/resources/styles_manager.dart';
@@ -37,7 +38,7 @@ class AnswerListView extends StatelessWidget {
         ),
         body: Column(
           children: [
-            CustomTabbar(),
+            AnswerListTabbar(),
             Expanded(
               child: TabBarView(
                 children: _views,
@@ -46,103 +47,6 @@ class AnswerListView extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class CustomTabbar extends StatefulWidget {
-  CustomTabbar({
-    Key? key,
-  }) : super(key: key);
-
-  static const List<Widget> _tabs = [
-    Tab(text: "전체"),
-    Tab(text: "책갈피"),
-  ];
-
-  @override
-  State<CustomTabbar> createState() => _CustomTabbarState();
-}
-
-class _CustomTabbarState extends State<CustomTabbar>
-    with TickerProviderStateMixin {
-  late TabController _tabController;
-  var currentTap = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(
-      length: 2,
-      vsync: this, //vsync에 this 형태로 전달해야 애니메이션이 정상 처리됨
-    );
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    //tabbar + underline container
-    return Column(
-      children: [
-        TabBar(
-          onTap: (index) {
-            setState(() {
-              currentTap = index;
-            });
-          },
-          controller: _tabController,
-          tabs: CustomTabbar._tabs,
-          //indicator style start
-          indicatorColor: ColorManager.point,
-          indicatorWeight: 2,
-          indicator: BoxDecoration(
-            color: ColorManager.point,
-            //배경 그라데이션 적용
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                ColorManager.white,
-                ColorManager.lightpoint,
-              ],
-            ),
-          ),
-          //indicator style end
-
-          //label style start
-          labelStyle: getMediumStyle(
-            color: ColorManager.darkGrey,
-            fontSize: AppSize.s16,
-          ),
-          labelColor: ColorManager.darkGrey,
-          unselectedLabelStyle: getMediumStyle(
-            color: ColorManager.point,
-            fontSize: AppSize.s16,
-          ),
-          unselectedLabelColor: ColorManager.lightGrey,
-          //label style end
-        ),
-        //underline container
-        Row(
-          children: [
-            Container(
-              color: currentTap == 0 ? ColorManager.point : null,
-              height: 2,
-              width: MediaQuery.of(context).size.width / 2,
-            ),
-            Container(
-              color: currentTap == 1 ? ColorManager.point : null,
-              height: 2,
-              width: MediaQuery.of(context).size.width / 2,
-            )
-          ],
-        ),
-      ],
     );
   }
 }
